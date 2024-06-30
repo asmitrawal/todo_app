70f29dc0f22c56f009ed7618478adcd7baffc489
Aayush Rawal <rawalasmit9@gmail.com>
2024-07-01 05:01:01 +0545

    firebase.exe removed from version control

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:todo_app/common/cubit/theme_mode_cubit.dart';
-import 'package:todo_app/common/my_theme_data.dart';
 import 'package:todo_app/common/my_theme_state.dart';
 import 'package:todo_app/dashboard/repository/dashboard_repository.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/login/repository/login_repository.dart';
 import 'package:todo_app/login/ui/login_screen.dart';
 
